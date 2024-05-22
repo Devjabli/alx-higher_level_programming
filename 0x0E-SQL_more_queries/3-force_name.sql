c5ac1cb41819ebdfa7b26e2b61b80a4a07f54131
Devjabli <aroussiotmane27@gmail.com>
2024-05-23 00:41:53 +0100

    3-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- creating table force_name on mysql server.
+-- force_name description: id INT, name VARCHAR(256) Not Null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
