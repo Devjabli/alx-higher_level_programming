ab504a2b0d5930fc244cd6636d49aaa9cc570fb2
Devjabli <aroussiotmane27@gmail.com>
2024-06-19 00:40:18 +0100

    check arguments process length

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const argc = process.argv.length
+const argc = process.argv.length;
 const checkArgc = 'Argument' + (argc > 3 ? 's' : '') + ' found';
 
 argc > 2 ? console.log(checkArgc) : console.log('No argument');
