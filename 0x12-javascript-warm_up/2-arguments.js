968671967ee3d0d43598d27339464c2127d32a63
Devjabli <aroussiotmane27@gmail.com>
2024-06-19 00:37:29 +0100

    check arguments process length

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const argc = process.argv.length
+const checkArgc = 'Argument' + (argc > 3 ? 's' : '') + ' found';
+
+argc > 2 ? console.log(checkArgc) : console.log('No argument');
