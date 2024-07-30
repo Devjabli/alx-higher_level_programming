49651b02b6d68ff3b8a2e18381ba9c59b1f209db
Devjabli <aroussiotmane27@gmail.com>
2024-07-31 00:51:06 +0100

    2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const req = require('request');
+const url = process.argv[2];
+
+req.get(url, (error, response) => {
+    error ? console.log(error) : console.log(`code: ${response.statusCode}`);
+});
