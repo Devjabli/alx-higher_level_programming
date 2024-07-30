4e1a356a4e1fd0663aa3713ac56b852cda98d06d
Devjabli <aroussiotmane27@gmail.com>
2024-07-30 23:55:37 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@
 const fs = require('fs');
 
 // Getting the filename from the command line args
-let filename = process.argv[2];
+const filename = process.argv[2];
 
 // Use fs.readfile toread file asynchronously
 fs.readFile(filename, 'utf-8', (err, content) => {
     // Check with ternary operator
     err ? console.log(err) : console.log(content);;
-})
+});
