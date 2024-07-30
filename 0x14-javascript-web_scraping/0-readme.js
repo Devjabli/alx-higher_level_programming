c24ae759807ea2c997f59d702b20fd3bec733a0b
Devjabli <aroussiotmane27@gmail.com>
2024-07-31 00:44:56 +0100

    update fix standard task 0 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 
-// Importing fs module for file system operations
-const fs = require('fs')
+const fs = require('fs');
 
-// Getting the filename from the command line args
-
-const filename = process.argv[2]
-// Use fs.readfile toread file asynchronously
+const filename = process.argv[2];
 
 fs.readFile(filename, 'utf-8', (err, content) => {
-  // Check with ternary operator
-  err ? console.log(err) : console.log(content)
-})
+  err ? console.log(err) : console.log(content);
+});
